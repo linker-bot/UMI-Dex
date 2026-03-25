f038466febbc78f0189a679f64e9bfacba6f7467
Su Yang <soulteary@gmail.com>
2026-03-25 21:44:49 +0800

    chore: Initialize the main program.

---
+++ src/controller_reader/setup.py
@@ -0,0 +1,32 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'controller_reader'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'),
+            glob(os.path.join('config', '*.yaml'))),
+    ],
+    install_requires=['setuptools', 'pyserial'],
+    zip_safe=True,
+    maintainer='ywt',
+    maintainer_email='ywt@todo.todo',
+    description='USB串口控制器数据读取节点，读取6轴控制器角度数据并发布ROS2话题',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'controller_reader_node = controller_reader.controller_reader_node:main'
+        ],
+    },
+)
---
+++ src/kimera_vio_bringup/setup.py
@@ -0,0 +1,34 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = "kimera_vio_bringup"
+
+setup(
+    name=package_name,
+    version="1.0.0",
+    packages=[],
+    data_files=[
+        (
+            "share/ament_index/resource_index/packages",
+            ["resource/" + package_name],
+        ),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            os.path.join("share", package_name, "launch"),
+            glob(os.path.join("launch", "*launch.[pxy][yma]*")),
+        ),
+        (
+            os.path.join("share", package_name, "config"),
+            glob(os.path.join("config", "*.yaml")),
+        ),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="ywt",
+    maintainer_email="ywt@todo.todo",
+    description="ROS2 bringup package for RealSense D455 and Kimera-VIO integration.",
+    license="MIT",
+    tests_require=["pytest"],
+    entry_points={},
+)
